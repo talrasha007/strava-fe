ddf26e89e7fb65357989b8bb952737fdec98e75b
Tal <talrasha007@gmail.com>
2019-12-10 22:16:07 +0800

    process token.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+const qs = require('querystring');
+const axios = require('axios');
 
 export default {
   name: 'app',
+  async beforeMount() {
+    let profile = JSON.parse(localStorage.getItem('strava') || 'null');
+    const query = qs.parse(location.search);
+
+    if (query.code && (!profile || profile.code !== query.code)) {
+      const { data } = await axios.get('/strava/oauth/get_token', { params: query });
+      profile = data;
+      localStorage.setItem('strava', JSON.stringify(profile));
+    }
+
+    if (!profile || profile.expires_at < Date.now() / 1000) {
+      location.href = `http://www.strava.com/oauth/authorize?client_id=41160&response_type=code&redirect_uri=${location.href}&approval_prompt=force&scope=read`;
+    }
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
